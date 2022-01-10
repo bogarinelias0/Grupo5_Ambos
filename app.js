fa1a0c63416e5df42241d58ecbfb68c66ac2360b
analuzalbornoz <analuzalbornoz@gmail.com>
2022-01-10 13:50:53 -0300

    agregando detalle del producto

--- app.js
+++ app.js
@@ -4,30 +4,49 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
 
-app.get('/carrito', (req,res)=>{
+app.get('/carrito', (req, res) => {
     res.sendFile(__dirname + '/views/productCart.html');
+
+});
+app.get('/detalleamboblanco', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail.html');
 });
 
-<<<<<<< HEAD
-app.get('/register', (req, res)=>{
+app.get('/detalleambofucsia', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail2.html');
+});
+
+app.get('/detalleambocombinado', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail3.html');
+});
+app.get('/detalleambogris', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail4.html');
+});
+app.get('/detalleamboceleste', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail5.html');
+});
+app.get('/detalleamborosado', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail6.html');
+});
+
+
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
-=======
-app.get('/productos', (req,res)=>{
+app.get('/productos', (req, res) => {
     res.sendFile(__dirname + '/views/catalogoDeProductos.html');
 });
 
->>>>>>> 2b4c6e0cd74928507097f0e8de67c9d3049d6219
 
-app.listen(process.env.PORT||3000, function(){
+app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo en puerto 3000");
 });
