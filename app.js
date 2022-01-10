4342e11d6a83d8acd43ae46d6a6994029bbc6ef4
FerNiveyro <ferni.maira@gmail.com>
2022-01-10 15:23:05 -0300

    Vinculando register y login con el home

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.get('/carrito', (req,res)=>{
     res.sendFile(__dirname + '/views/productCart.html');
 });
 
-<<<<<<< HEAD
 app.get('/register', (req, res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
@@ -21,12 +20,10 @@ app.get('/register', (req, res)=>{
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
-=======
 app.get('/productos', (req,res)=>{
     res.sendFile(__dirname + '/views/catalogoDeProductos.html');
 });
 
->>>>>>> 2b4c6e0cd74928507097f0e8de67c9d3049d6219
 
 app.listen(process.env.PORT||3000, function(){
     console.log("Servidor corriendo en puerto 3000");
