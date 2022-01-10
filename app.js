449b564d18a8fa41177feb6e60c5ce94f51a205e
FerNiveyro <ferni.maira@gmail.com>
2022-01-10 08:49:34 -0300

    Añado register y login

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/carrito', (req,res)=>{
     res.sendFile(__dirname + '/views/productCart.html');
 });
 
+app.get('/register', (req, res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
 app.listen(process.env.PORT||3000, function(){
     console.log("Servidor corriendo en puerto 3000");
 });
