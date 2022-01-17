8268ecb1da8145489ecf92503329e126fe193f3f
Nico Gallardo <alfredongallg@gmail.com>
2022-01-17 07:43:38 -0300

    Se configura el template engines EJS

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static("public"));
+//Config de EJS Template Engine
+
+app.set("view engine","ejs")
+
+app.use(express.static("../public"));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
